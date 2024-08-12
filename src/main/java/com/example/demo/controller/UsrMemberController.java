67d702f36df8c40cfa9441302152ef9c6257c78f
ãs3496h <s3496h@gmail.com>
2024-08-12 13:30:27 +0900

    입력된 데이터의 null 체크 함수 구현

--- src/main/java/com/example/demo/controller/UsrMemberController.java
+++ src/main/java/com/example/demo/controller/UsrMemberController.java
@@ -1,11 +1,13 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.service.MemberService;
+import com.example.demo.util.Ut;
 import com.example.demo.vo.Member;
 
 @Controller
@@ -16,10 +18,36 @@ public class UsrMemberController {
 
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public Member doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
+	public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum,
 			String email) {
+
+		if (Ut.isEmptyOrNull(loginId)) {
+			return "loginId를 입력 해주세요";
+		}
+		if (Ut.isEmptyOrNull(loginPw)) {
+			return "loginPw를 입력 해주세요";
+		}
+		if (Ut.isEmptyOrNull(name)) {
+			return "name를 입력해주세요";
+		}
+		if (Ut.isEmptyOrNull(nickname)) {
+			return "nickname를 입력해주세요";
+		}
+		if (Ut.isEmptyOrNull(cellphoneNum)) {
+			return "cellphoneNum를 입력해주세요";
+		}
+		if (Ut.isEmptyOrNull(email)) {
+			return "email를 입력해주세요";
+		}
+
 		int id = memberService.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
+
+		if (id == -1) {
+			return "이미 사용중인 아이디";
+		}
+
 		Member member = memberService.getMemberById(id);
+
 		return member;
 	}
 
