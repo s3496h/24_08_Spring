2540013feaadd21458ba1a13cc74d80b5bce90a6
s3496h <s3496h@gmail.com>
2024-08-13 11:15:57 +0900

    수정,삭제 권한체크, service에서 권한체크 하도록

--- src/main/java/com/example/demo/service/ArticleService.java
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -44,5 +44,14 @@ public class ArticleService {
 	public List<Article> getArticles() {
 		return articleRepository.getArticles();
 	}
+	
+	public ResultData loginedMemberCanModify(int loginedMemberId, Article article) {
+
+		if (article.getMemberId() != loginedMemberId) {
+			return ResultData.from("F-2", Ut.f("%d번 게시글에 대한 권한이 없습니다", article.getId()));
+
+		}
+		return ResultData.from("S-1", Ut.f("%d번 게시글을 수정했습니다", article.getId()), article);
+	}
 
 }
